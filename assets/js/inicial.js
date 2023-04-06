052ce9ef4893eeba7089e0255ea01c98debbafe5
Andre <105212500+andrelqvalpassos@users.noreply.github.com>
2023-04-06 17:40:26 -0300

    att4

--- assets/js/inicial.js
+++ assets/js/inicial.js
@@ -1,19 +1,4 @@
-// Seleciona o botão "Fazer uma aposta" na seção principal
-const mainButton = document.querySelector('main button');
-
-// Adiciona um evento de clique no botão
-mainButton.addEventListener('click', function() {
-  // Abre uma nova janela com a página de apostas
-  window.open('https://www.site-de-apostas.com/apostas', '_blank');
-});
-
-// Seleciona todos os botões "Fazer uma aposta" nas seções de eventos
-const eventButtons = document.querySelectorAll('section ul li button');
-
-// Adiciona um evento de clique em cada botão de evento
-eventButtons.forEach(function(button) {
-  button.addEventListener('click', function() {
-    // Exibe uma mensagem informando que a aposta foi realizada com sucesso
-    alert('Sua aposta foi realizada com sucesso!');
-  });
-});
+function alert(){
+  Swal.fire('Solicitação enviada')
+}
+alert();
